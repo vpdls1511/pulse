cf9a8553a492bf816de7bb725b492c4e6b4717a5
nGyu <vpdls1511@gmail.com>
2026-01-14 22:27:55 +0900

    feat : 요청에대한 응답 가능

--- src/main/java/me/ngyu/pulse/server/PulseServer.java
+++ src/main/java/me/ngyu/pulse/server/PulseServer.java
@@ -1,6 +1,9 @@
 package me.ngyu.pulse.server;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -18,8 +21,25 @@ public class PulseServer {
 
     while (true) {
       Socket socket = serverSocket.accept();
-      socket.close();
+
+      handleRequest(socket);
     }
   }
 
+  private void handleRequest(Socket socket) throws IOException {
+    BufferedReader reader = new BufferedReader(
+      new InputStreamReader(socket.getInputStream())
+    );
+
+    String line;
+    while ((line = reader.readLine()) != null && !line.isEmpty()) {
+      System.out.println(line);
+    }
+
+    OutputStream outputStream = socket.getOutputStream();
+    outputStream.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+
+    outputStream.flush();
+    socket.close();
+  }
 }
