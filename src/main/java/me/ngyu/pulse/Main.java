8871269bb2acb41da4dd983adc4fc581253a058e
nGyu <vpdls1511@gmail.com>
2026-01-21 20:59:24 +0900

    feat : Component Scan 동작

--- src/main/java/me/ngyu/pulse/Main.java
+++ src/main/java/me/ngyu/pulse/Main.java
@@ -1,11 +1,24 @@
 package me.ngyu.pulse;
 
+import me.ngyu.pulse.core.container.BeanContainer;
+import me.ngyu.pulse.core.scanner.ComponentScanner;
 import me.ngyu.pulse.server.PulseServer;
 
-import java.io.IOException;
-
 public class Main {
-  public static void main(String[] args) throws IOException {
+
+  private static final String BASE_PACKAGE = "me.ngyu.pulse";
+
+  public static void main(String[] args) throws Exception {
+    BeanContainer beancontainer = new BeanContainer();
+    ComponentScanner scanner = new ComponentScanner(beancontainer);
+
+    scanner.scan(BASE_PACKAGE);
+
+    System.out.println("\uD83D\uDCE6 Registered Beans: ");
+    beancontainer.getAllBeans().forEach((clazz, instance) -> {
+      System.out.println("- " + clazz.getSimpleName());
+    });
+
     new PulseServer(8080).run();
   }
 }
