8c49b10f1fd21ffcf56649ce8e8e099bef429a09
nGyu <vpdls1511@gmail.com>
2026-01-26 21:31:53 +0900

    test : DI테스트

--- src/main/java/me/ngyu/pulse/handler/PingHandler.java
+++ src/main/java/me/ngyu/pulse/handler/PingHandler.java
@@ -1,13 +1,19 @@
 package me.ngyu.pulse.handler;
 
+ import me.ngyu.pulse.core.annotation.Autowired;
 import me.ngyu.pulse.core.annotation.Component;
+import me.ngyu.pulse.handler.service.TestService;
 
 @Component
 public class PingHandler {
 
+  @Autowired
+  private TestService testService;
+
   public PingHandler() {}
 
   public String getResponse() {
+    testService.test();
     return "<h1> Ping from DI Container</h1>";
   }
 }
--- src/main/java/me/ngyu/pulse/handler/service/TestService.java
+++ src/main/java/me/ngyu/pulse/handler/service/TestService.java
@@ -1,4 +1,11 @@
 package me.ngyu.pulse.handler.service;
 
+import me.ngyu.pulse.core.annotation.Component;
+
+@Component
 public class TestService {
+
+  public void test() {
+    System.out.println("test");
+  }
 }
